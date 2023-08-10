f08eafd03b276f9f2a38e59bdab98a1efbcc8c18
yinkusadel <adeleyeolayinka73@gmail.com>
2023-08-10 04:58:34 +0100

    feat:add complete solution to basketballscoreboard

---
+++ script.js
@@ -0,0 +1,46 @@
+const homeScore = document.getElementById('home-score');
+const guestScore = document.getElementById('guest-score');
+let currentHomeScore = 0;
+let currentGuestScore = 0;
+
+function oneGoalToHome() {
+  homeScore.textContent = `${(currentHomeScore += 1)}`;
+}
+
+function twoGoalToHome() {
+  homeScore.textContent = `${(currentHomeScore += 2)}`;
+}
+
+function threeGoalToHome() {
+  homeScore.textContent = `${(currentHomeScore += 3)}`;
+}
+
+function oneGoalToGuest() {
+  guestScore.textContent = `${(currentGuestScore += 1)}`;
+}
+
+function twoGoalToGuest() {
+  guestScore.textContent = `${(currentGuestScore += 2)}`;
+}
+
+function threeGoalToGuest() {
+  guestScore.textContent = `${(currentGuestScore += 3)}`;
+}
+
+const homeOne = document.getElementById('home-btn-1');
+homeOne.onclick = oneGoalToHome;
+
+const homeTwo = document.getElementById('home-btn-2');
+homeTwo.onclick = twoGoalToHome;
+
+const homeThree = document.getElementById('home-btn-3');
+homeThree.onclick = threeGoalToHome;
+
+const guestOne = document.getElementById('guest-btn-1');
+guestOne.onclick = oneGoalToGuest;
+
+const guestTwo = document.getElementById('guest-btn-2');
+guestTwo.onclick = twoGoalToGuest;
+
+const guestThree = document.getElementById('guest-btn-3');
+guestThree.onclick = threeGoalToGuest;
